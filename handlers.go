c753d78da95193e889ceb17960979f56ca95c308
Anna Korunska <36419787+akorunska@users.noreply.github.com>
2020-03-10 11:58:03 +0000

    post request to /files with json data now echoes whatever is in "contents" field

--- handlers.go
+++ handlers.go
@@ -3,10 +3,15 @@ package main
 import (
     "fmt"
     "io/ioutil"
+    "encoding/json"
     "log"
     "net/http"
     "path/filepath"
 )
+
+type FileContent struct {
+  Contents string `json:"contents"`
+}
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
     // handles static html-s for the predefined paths
@@ -21,11 +26,32 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 
 func filesHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
-    case http.MethodGet:
-       fmt.Fprintf(w, "get method")
-    case http.MethodPost:
+      case http.MethodGet:
         fmt.Fprintf(w, "post method")
-    default:
+      case http.MethodPost:
+        fileContent := FileContent{}
+
+        b, err := ioutil.ReadAll(r.Body)
+        defer r.Body.Close()
+        if err != nil {
+          http.Error(w, err.Error(), 500)
+          return
+        }
+
+        err = json.Unmarshal(b, &fileContent)
+        if err != nil {
+          http.Error(w, err.Error(), 500)
+          return
+        }
+
+        output, err := json.Marshal(fileContent)
+        if err != nil {
+          http.Error(w, err.Error(), 500)
+          return
+        }
+        w.Header().Set("content-type", "application/json")
+        w.Write(output)
+      default:
         fmt.Fprintf(w, "method not recognised")
     }
 }
